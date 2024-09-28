9f047bcc42e35060ebe25976e7b073d698620bab
pbananas <itspetebananas@gmail.com>
2024-09-27 21:54:07 -0400

    testing snapshot script again

--- bin/snapshot.sh
+++ bin/snapshot.sh
@@ -2,12 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
 
-if ! git diff-index --quiet HEAD --; then
+if git diff-index --quiet HEAD --; then
   echo "No changes."
   exit 0
 fi
 
-MSG="${1:-wip}"
+MSG="${@:-wip}"
 
 git add .
 git commit -m "$MSG"
